a1e5b2a1ade1714ecfdb52429b549b2fea4575f2
Matheus Carneiro <carneiromatheu@gmail.com>
2025-01-28 10:36:02 -0300

    fix: Pegar palavras compostas e frases na word2 do command

--- Parser.java
+++ Parser.java
@@ -24,8 +24,8 @@ public class Parser
         Scanner tokenizer = new Scanner(inputLine);
         if(tokenizer.hasNext()) {
             word1 = tokenizer.next();
-            if(tokenizer.hasNext()) {
-                word2 = tokenizer.next();
+            if(tokenizer.hasNextLine()) {
+                word2 = tokenizer.nextLine().trim();
             }
         }
 
